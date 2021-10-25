8f80f3f2374d558f260f85584e071f8995838643
Jakhongir <72187687+JakhongirMatkulov@users.noreply.github.com>
2021-10-25 19:42:26 +0500

    solved even or odd number example

--- Easy algorithms/main.js
+++ Easy algorithms/main.js
@@ -1,1 +1,11 @@
 ////start
+
+// Find the given number of 21, odd or even.
+
+const givenNum = 22;
+
+if (givenNum % 2 === 0) { // 
+  console.log(givenNum + ' is even number');
+} else {
+  console.log(givenNum + ' is odd number');
+}
